9d1a5877fd6d40e13030dde13f1bc27ede4831da
Ricardo <ricardoarrobo@gmail.com>
2020-03-18 18:08:20 -0500

    route updated

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-
-})
+app.get('/:name', (req, res) => {
+    let { name } = req.params;
+    res.send(`Hello ${name}`);
+});
 
 app.listen(port);
 console.log("Server started at port " + port);
